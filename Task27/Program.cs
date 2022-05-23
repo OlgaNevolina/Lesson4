e63e540d5fd0ced142c7ea651b38b55daefdce88
nevol78@mail.ru <nevol78@mail.ru>
2022-05-24 01:19:02 +0300

    Task27

---
+++ Task27/Program.cs
@@ -0,0 +1,13 @@
+﻿Console.Write("Введите целое число: ");
+int num = int.Parse(Console.ReadLine());
+int sum = 0;
+int SumNum (int num)
+{
+    for (int n = num; n > 0; sum += n % 10, n /= 10 )
+    {
+        sum = sum * 1;
+    }
+    return sum;
+}
+int result = SumNum(num);
+Console.WriteLine($"Сумма цифр числа = {result}");
