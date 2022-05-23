4e95e4922f93d5b58246b920a4d8f64bf4879bc8
nevol78@mail.ru <nevol78@mail.ru>
2022-05-24 01:15:52 +0300

    Task25

---
+++ Task25/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите целое число А: ");
+int numA = int.Parse(Console.ReadLine());
+Console.Write("Введите натуральное число В: ");
+int numB = int.Parse(Console.ReadLine());
+int sum = 1;
+for (int i = 0; i < numB; i++)
+{
+    sum *= numA;
+}
+Console.WriteLine($"А в степени В = {sum}");
